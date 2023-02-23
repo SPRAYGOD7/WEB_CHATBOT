a230fb534393fdd6b139bf39949a3dfb292fc63b
SPRAYGOD7 <102588403+SPRAYGOD7@users.noreply.github.com>
2023-02-23 11:46:20 +0530

    Add files via upload

---
+++ WEB_CHATBOT/static/scripts/responses.js
@@ -0,0 +1,35 @@
+function getBotResponse(input) {
+
+    if (input == "rock") {
+        return "paper";
+    } else if (input == "paper") {
+        return "scissors";
+    } else if (input == "scissors") {
+        return "rock";
+    }
+
+
+    if (input == "hello") {
+        return "Hello there!";
+    } else if (input == "goodbye") {
+        return "Talk to you later!";
+    } else if (input == "help"){
+        return "Yup i'am here to help you";
+    } else if (input == "bye"){
+        return "See you soon 👋️";}
+        
+
+      
+      
+      
+     
+      
+      
+      
+      
+      
+      
+       else ("")
+        return  ("try something else")
+   
+    }
